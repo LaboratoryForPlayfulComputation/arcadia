69c0f686426232a73e3c29fdccbe2d2fb553ba27
shadowfax6894 <anniekelly6894@gmail.com>
2017-06-20 16:59:08 -0700

    adding timbre require

--- sim/music.ts
+++ sim/music.ts
@@ -1,5 +1,9 @@
+/// <reference path="../node_modules/pxt-core/typings/globals/node/index.d.ts"/>
+/// <reference path="../node_modules/pxt-core/built/pxtlib.d.ts" />
+
 namespace pxsim.music {
 
-
+    export let T = require("timbre");
+    //T("sin", {freq:880, mul:0.5}).play();
 
 }
