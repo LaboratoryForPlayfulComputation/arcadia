9a203d5746ff76037e9f68bb2a9fdd65e655700c
shadowfax6894 <anniekelly6894@gmail.com>
2017-11-29 14:31:42 -0700

    network change

--- sim/network.ts
+++ sim/network.ts
@@ -15,7 +15,9 @@ namespace pxsim.network {
         }            
 
         if (!ws){
-            ws = new WebSocket("ws://localhost:4244");
+            // look at url for current page and insert hostname (instead of redonkadonk)
+            let urlString = "wss://" + window.location.hostname + ":8677";
+            ws = new WebSocket(urlString);
             return new Promise<string>((resolve, reject) => {
                 ws.onopen = function() {
                     let wsMessage = JSON.stringify({"ip": ip, "port": port, "addr": addr, "msg": newArgs});
